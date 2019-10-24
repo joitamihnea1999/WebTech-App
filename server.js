951dcc58e481a5531c5d7b26d8abdc1d97a62304
Ubuntu <ubuntu@ip-172-31-46-54.eu-central-1.compute.internal>
2019-10-24 07:23:10 +0000

    first commit

--- server.js
+++ server.js
@@ -15,6 +15,30 @@ app.get('/contact', function(request, response){
     response.type('text/html').send(content)
 })
 
+// GET /messages
+app.get('/messages', function(request, response){
+  var messages = [];
+  var message = {
+    name: "Joita",
+    message: "Te saluta!"
+  }
+  messages.push(message);
+  
+  response.send(messages); 
+})
+
+// GET /messages/:id
+app.get('/messages/:id', function(request, response){
+  response.send('Am apelat GET /messages/' + request.params.id);  
+})
+
+// POST /messages
+app.post('/messages', function(request,response){
+  response.send('Am apelat POST /messages');
+})
+
+// PUT /messages/:id
+// DELETE //mesages/:id
 
 
 app.listen(3000)
